d79d73fe4549fc08c37e0632fae504e1c6aea9d4
이하영 <lhy0530@khu.ac.kr>
2021-01-08 01:44:01 +0900

    [14681]사분면 고르기

---
+++ 14681.py
@@ -0,0 +1,13 @@
+def sol(x,y):
+    if x>0 and y>0:
+        return 1
+    elif x<0 and y>0:
+        return 2
+    elif x<0 and y<0:
+        return 3
+    else:
+        return 4
+
+x=int(input())
+y=int(input())
+print(sol(x,y))
